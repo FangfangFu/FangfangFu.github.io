bebd7a771b45e4e96ef99b80ba8cf020bad4b870
Fangfang Fu <fangfang@fufangfang.com>
2016-03-10 15:40:47 -0600

    Fix navbar and layout

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,3 +8,13 @@ button_click("#email", "http://mail.google.com", "mail");
 button_click("#facebook", "http://www.facebook.com/fangfang.fu.315", "facebook");
 button_click("#qq", "http://user.qzone.qq.com/450490365", "qq");
 
+$( ".slidedown li" ).hide();
+$(".contact").click(function () {
+  if ( $( ".slidedown li:first" ).is( ":hidden" ) ) {
+    $( ".slidedown li" ).slideDown( "slow" );
+    $(".contact").addClass("contact_background");
+  } else {
+    $( ".slidedown li" ).hide();
+    $(".contact").removeClass("contact_background");
+  }
+});
